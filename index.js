97bf56be00357e70b6d11f9dbcba8651e6978002
root <root@Ty-Laptop.localdomain>
2022-12-10 13:49:22 -0500

    working 12/10

--- index.js
+++ index.js
@@ -1,60 +1,110 @@
-import {API_KEY} from "/api.js";
-let CityPrompt = prompt('Please enter city name');
+import {API_KEY, UNSPLASH_KEY} from "/api.js";
+let city, state;
+
+//add elements to HTML page
 let listOfWeather = document.createElement('ul');
-let cityInfo = document.createElement('h1');
-cityInfo.innerText = `${CityPrompt.toUpperCase()}`
+let cityInfo = document.createElement('li');
 listOfWeather.append(cityInfo);
 document.body.append(listOfWeather);
-
-
-
+let form = document.querySelector('#enterInfo');
+form.addEventListener('submit', function(event) {
+event.preventDefault();
+ city = event.target.city.value;
+ state = event.target.state.value;
+ getLatLon(city, state);
+ event.target.reset();
+});
+let btn = document.querySelector('#btn');
+btn.addEventListener('click', function() {
+  form.style.display = 'none';
+})
 
 //first gets latitude and logitude from entered city, then get weather info from latitude and logitude
-function getLatLon(test) {
-  let city = test;
+function getLatLon(city, state) {
 fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`)
   .then(responce => responce.json())
-  .then(data => data.forEach(element => {
-      if(element.state === "Florida") {
-        let lat = element.lat;
-        let lon = element.lon;
+  .then(data =>  {
+    let findState = data.find(obj => obj.state.toUpperCase() === state.toUpperCase())  
+        let lat = findState.lat;
+        let lon = findState.lon;
         return fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,daily&appid=${API_KEY}`)
         .then(responce => responce.json())
-        .then(data => renderALL(data))
+        .then(data => {
+          renderALL(data, city, state);
+        })
+
       }
-  }));
-};    
-getLatLon(CityPrompt);
-
-
-
+  );
+};  
 function renderTeampHeader(data) {
-  let tempData = document.createElement('h2');
+  let tempData = document.createElement('li');
+  tempData.className = 'weather-list';
   tempData.innerText = `Temp ${Math.round(data.current.temp)}`;
   listOfWeather.append(tempData);
 }
 function renderFeelsLike(data) {
-  let feelsData = document.createElement('h2');
+  let feelsData = document.createElement('li');
+  feelsData.className = 'weather-list';
   feelsData.innerText = `Feels like ${Math.round(data.current.feels_like)}`;
   listOfWeather.append(feelsData);
 }
 function renderHumidity(data) {
-  let humidity = document.createElement('h2');
+  let humidity = document.createElement('li');
+  humidity.className = 'weather-list';
   humidity.innerText = `Humidity ${data.current.humidity}`;
   listOfWeather.append(humidity);
 }
 function renderAtomosphere(data) {
-  let atomosphere = document.createElement('h2');
+  let atomosphere = document.createElement('li');
+  atomosphere.className = 'weather-list';
   atomosphere.innerText = `${data.current.weather[0].description}`;
   listOfWeather.append(atomosphere);
+  getDateAndTime(data.timezone, atomosphere.textContent)
 }
-function renderALL(data) {
+function getDateAndTime(timeZone, climate) {
+  return fetch(`http://worldtimeapi.org/api/timezone/${timeZone}`)
+  .then(r => r.json())
+  .then(d => {
+    let date = document.createElement('li');
+    let hours = +d.datetime.slice(11).split('.')[0].slice(0, 5).split(':')[0];
+    let minutes = +d.datetime.slice(11).split('.')[0].slice(0, 5).split(':')[1];    
+    if(hours >= 13) {
+      date.innerText = `${d.datetime.slice(0, 10).slice(5)} ${hours - 12}:${minutes}pm`;
+      date.id = 'date-and-time';
+      cityInfo.append(date);
+    } else {
+      date.innerText = `${d.datetime.slice(0, 10).slice(5)} ${hours}:${minutes}am`;
+      date.id = 'date-and-time';
+      cityInfo.append(date);
+    }
+    setBackground(timeZone, climate);
+  })}
+
+
+function setBackground(timeZone, climate) {
+  return fetch(`http://worldtimeapi.org/api/timezone/${timeZone}`)
+  .then(r => r.json())
+  .then(d => {
+    let hours = +d.datetime.slice(11).split('.')[0].slice(0, 5).split(':')[0];
+    let background = document.querySelector("#backgroundIMG");
+    if(city && state) {
+      fetch(`https://api.unsplash.com/search/photos?page=1&query=${climate}&location[city]=${city}&client_id=${UNSPLASH_KEY}`)
+        .then(response => response.json())
+        .then(data => {
+           background.src = data.results[Math.floor(Math.random() * 8)].urls.full;
+        })
+      }
+    })
+}  
+
+
+
+function renderALL(data, genCity, genState) {
+  cityInfo.textContent = `${genCity.toUpperCase()}, ${genState.toUpperCase()}`;
+  cityInfo.id = 'city-name-state-name';
   renderTeampHeader(data);
   renderFeelsLike(data);
   renderHumidity(data);
   renderAtomosphere(data);
 }
-
-
-
 
