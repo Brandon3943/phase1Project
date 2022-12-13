c8a5abbd8a910d3adfa0e73f3f0b732e908a2bde
root <root@Ty-Laptop.localdomain>
2022-12-12 21:11:01 -0500

    completed pahse1 project

--- index.js
+++ index.js
@@ -14,10 +14,6 @@ event.preventDefault();
  getLatLon(city, state);
  event.target.reset();
 });
-let btn = document.querySelector('#btn');
-btn.addEventListener('click', function() {
-  form.style.display = 'none';
-})
 
 //first gets latitude and logitude from entered city, then get weather info from latitude and logitude
 function getLatLon(city, state) {
@@ -27,36 +23,42 @@ fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${AP
     let findState = data.find(obj => obj.state.toUpperCase() === state.toUpperCase())  
         let lat = findState.lat;
         let lon = findState.lon;
+        if(lat && lon) {
         return fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=hourly,daily&appid=${API_KEY}`)
         .then(responce => responce.json())
         .then(data => {
           renderALL(data, city, state);
+          form.style.display = 'none';          
         })
+        } else {
+          return "Error"
+        }
+    }  
+  )
+  .catch(() => alert("Unexpected error, please try search again."))
+}; 
 
-      }
-  );
-};  
 function renderTeampHeader(data) {
   let tempData = document.createElement('li');
   tempData.className = 'weather-list';
-  tempData.innerText = `Temp ${Math.round(data.current.temp)}`;
+  tempData.innerText = `Temp ${Math.round(data.current.temp)}𝇈`;
   listOfWeather.append(tempData);
 }
 function renderFeelsLike(data) {
   let feelsData = document.createElement('li');
-  feelsData.className = 'weather-list';
-  feelsData.innerText = `Feels like ${Math.round(data.current.feels_like)}`;
+  feelsData.className = 'weather-list1';
+  feelsData.innerText = `Feels like ${Math.round(data.current.feels_like)}𝇈`;
   listOfWeather.append(feelsData);
 }
 function renderHumidity(data) {
   let humidity = document.createElement('li');
-  humidity.className = 'weather-list';
-  humidity.innerText = `Humidity ${data.current.humidity}`;
+  humidity.className = 'weather-list2';
+  humidity.innerText = ` ${data.current.humidity}% humidity`;
   listOfWeather.append(humidity);
 }
 function renderAtomosphere(data) {
   let atomosphere = document.createElement('li');
-  atomosphere.className = 'weather-list';
+  atomosphere.className = 'weather-list3';
   atomosphere.innerText = `${data.current.weather[0].description}`;
   listOfWeather.append(atomosphere);
   getDateAndTime(data.timezone, atomosphere.textContent)
@@ -65,6 +67,7 @@ function getDateAndTime(timeZone, climate) {
   return fetch(`http://worldtimeapi.org/api/timezone/${timeZone}`)
   .then(r => r.json())
   .then(d => {
+    let goodDate = new Date(d.datetime)
     let date = document.createElement('li');
     let hours = +d.datetime.slice(11).split('.')[0].slice(0, 5).split(':')[0];
     let minutes = +d.datetime.slice(11).split('.')[0].slice(0, 5).split(':')[1];    
@@ -85,9 +88,8 @@ function setBackground(timeZone, climate) {
   return fetch(`http://worldtimeapi.org/api/timezone/${timeZone}`)
   .then(r => r.json())
   .then(d => {
-    let hours = +d.datetime.slice(11).split('.')[0].slice(0, 5).split(':')[0];
     let background = document.querySelector("#backgroundIMG");
-    if(city && state) {
+    if(city) {
       fetch(`https://api.unsplash.com/search/photos?page=1&query=${climate}&location[city]=${city}&client_id=${UNSPLASH_KEY}`)
         .then(response => response.json())
         .then(data => {
